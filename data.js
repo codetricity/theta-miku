3043cbc6d75ac8dd6b7f229d21cead79b8f37df2
Craig <craigoda@gmail.com>
2020-05-18 17:08:07 -0700

    first commit

---
+++ data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20200402_135034",
+      "name": "20200402_135034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.017318463591307,
+        "pitch": 0.16381093499169985,
+        "fov": 1.4676406701751494
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9709846085410643,
+          "pitch": 0.13560592593685605,
+          "rotation": 0,
+          "target": "1-20200402_135739"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0636462417220365,
+          "pitch": 0.15525870892716043,
+          "title": "Add Characters",
+          "text": "RICOH THETA SC Hatsune Miku special edition camera with custom software"
+        }
+      ]
+    },
+    {
+      "id": "1-20200402_135739",
+      "name": "20200402_135739",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.20302820114052267,
+        "pitch": -0.7642169637882716,
+        "fov": 1.4676406701751494
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0547999086538677,
+          "pitch": -0.20629505671307946,
+          "rotation": 4.71238898038469,
+          "target": "0-20200402_135034"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-20200402_141853",
+      "name": "20200402_141853",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Hatsune Hanami",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
